e958087b24c612539e0640bb1f94a278ec4408dd
shahram <shahram@newrelic.com>
2023-05-08 00:18:41 -0700

    add kpi bar to components index and styles files

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ export { default as EditStepModal } from './edit-step-modal';
 export { default as Signal } from './signal';
 export { default as SignalsList } from './signals-list';
 export { default as KpiModal } from './kpi-modal';
+export { default as KpiBar } from './kpi-bar';
 
 // export { IconsLib, NoFlows };
