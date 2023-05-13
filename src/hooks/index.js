8dc005455214cfc701af90a31be23ccf543412b2
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2023-05-12 18:47:08 -0700

    refactor: useFetchKpis

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,4 @@
 export { default as useFetchFlows } from './use-fetch-flows';
 export { default as useServiceLevelsSearch } from './use-service-levels-search';
 export { default as useFetchServiceLevels } from './use-fetch-service-levels';
-export { default as useFetchKpiValues } from './use-fetch-kpi-values';
+export { default as useFetchKpis } from './use-fetch-kpis';
---
+++ src/hooks/use-fetch-kpis/index.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { useNerdGraphQuery } from 'nr1';
+
+import { queriesGQL } from '../../queries';
+
+const valueFromResult = (result = {}) => {
+  const { comparison, ...vals } = result;
+  const values = Object.values(vals);
+  if (values.length === 1) {
+    const [value] = values;
+    if (!comparison) return { value };
+    if (comparison === 'current') return { value };
+    if (comparison === 'previous') return { previousValue: value };
+  }
+};
+
+const kpisFromData = ({ actor = {} } = {}) =>
+  Object.keys(actor).reduce((acc, key) => {
+    if (!key.startsWith('q')) return acc;
+    const index = key.substring(1);
+    const { results: [first, second, ...rest] = [] } = actor[key];
+    if (!rest.length) {
+      acc[index] = {
+        value: '',
+        previousValue: '',
+        ...valueFromResult(first),
+        ...valueFromResult(second),
+      };
+    }
+    return acc;
+  }, []);
+
+const useFetchKpis = ({ kpiData = [] } = {}) => {
+  const [kpis, setKpis] = useState([]);
+  const [query, setQuery] = useState(queriesGQL());
+  const { data, error, loading } = useNerdGraphQuery({ query });
+
+  useEffect(() => {
+    if (kpiData?.length) {
+      const queries = kpiData.map(
+        ({ accountIds = [], nrqlQuery = '' }, index) => ({
+          accounts: accountIds.join(', '),
+          alias: `q${index}`,
+          query: nrqlQuery,
+        })
+      );
+      setQuery(queriesGQL(queries));
+    }
+  }, [kpiData]);
+
+  useEffect(() => {
+    if (data && !loading) {
+      setKpis(kpisFromData(data));
+    }
+  }, [data, loading]);
+
+  useEffect(() => {
+    if (error) {
+      console.error('Error fetching Kpi values', error);
+    }
+  }, [error]);
+
+  return { kpis, error, loading };
+};
+
+export default useFetchKpis;
