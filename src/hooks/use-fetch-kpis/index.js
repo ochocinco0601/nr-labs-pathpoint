b09156854d2448b51f12c03de308e8bb2280f9eb
shahram <shahram@newrelic.com>
2023-05-14 16:54:39 -0700

    fix: null actor kyes

--- src/hooks/use-fetch-kpis/index.js
+++ src/hooks/use-fetch-kpis/index.js
@@ -16,7 +16,7 @@ const valueFromResult = (result = {}) => {
 
 const kpisFromData = ({ actor = {} } = {}) =>
   Object.keys(actor).reduce((acc, key) => {
-    if (!key.startsWith('q')) return acc;
+    if (!key.startsWith('q') || !actor[key]) return acc;
     const index = key.substring(1);
     const { results: [first, second, ...rest] = [] } = actor[key];
     if (!rest.length) {
