efae155f032a8e6fb7a759456227e66fd28a2186
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2023-05-17 11:55:15 -0700

    feat: step component

---
+++ src/components/step/index.js
@@ -0,0 +1,49 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+
+import { HeadingText } from 'nr1';
+import { StatusIconsLayout } from '@newrelic/nr-labs-components';
+
+import Signal from '../signal';
+import { signalStatus } from '../../utils';
+import { MODES } from '../../constants';
+
+const Step = ({ signals = [], title = 'Step', mode = MODES.KIOSK }) => {
+  const SignalsGrid = memo(
+    () => (
+      <StatusIconsLayout
+        statuses={signals.map((signal) => ({
+          status: signalStatus(signal),
+        }))}
+      />
+    ),
+    [signals]
+  );
+  SignalsGrid.displayName = 'SignalsGrid';
+
+  const SignalsList = memo(
+    () => signals.map((signal, i) => <Signal key={i} {...signal} />),
+    [signals]
+  );
+  SignalsList.displayName = 'SignalsList';
+
+  return (
+    <div className="step">
+      <HeadingText type={HeadingText.TYPE.HEADING_6} className="title">
+        {title}
+      </HeadingText>
+      <div className="signals">
+        {mode === MODES.KIOSK ? <SignalsGrid /> : null}
+        {mode === MODES.LIST ? <SignalsList /> : null}
+      </div>
+    </div>
+  );
+};
+
+Step.propTypes = {
+  signals: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string,
+  mode: PropTypes.oneOf(Object.values(MODES)),
+};
+
+export default Step;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
 export * from './nerd-storage';
+export * from './modes';
