dcaef7d5cae7ee36ca576c40e8f25fdc693365a3
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2023-05-22 10:41:19 -0700

    refactor: step signals

--- src/components/signal/index.js
+++ src/components/signal/index.js
@@ -1,28 +1,18 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { StatusIcon } from '@newrelic/nr-labs-components';
-import { SIGNAL_TYPES, signalStatus } from '../../utils';
 
-const Signal = ({ type = '', name, ...params }) => {
-  const status = useMemo(
-    () => signalStatus({ type, ...params }),
-    [type, params]
-  );
-
-  return (
-    <div className="signal">
-      <StatusIcon status={status} />
-      <span className="name">{name}</span>
-    </div>
-  );
-};
+const Signal = ({ name, status }) => (
+  <div className="signal">
+    <StatusIcon status={status} />
+    <span className="name">{name}</span>
+  </div>
+);
 
 Signal.propTypes = {
-  type: PropTypes.oneOf(Object.values(SIGNAL_TYPES)),
   name: PropTypes.string,
-  attainment: PropTypes.number, // for type === service_level
-  target: PropTypes.number, // for type === service_level
+  status: PropTypes.oneOf(Object.values(StatusIcon.STATUSES)),
 };
 
 export default Signal;
--- src/utils/signal.js
+++ src/utils/signal.js
@@ -2,16 +2,29 @@ import { StatusIcon } from '@newrelic/nr-labs-components';
 
 import { serviceLevelStatus } from './service-levels';
 
-const {
-  STATUSES: { UNKNOWN },
-} = StatusIcon;
+const { STATUSES } = StatusIcon;
+
+const statuses = [
+  STATUSES.UNKNOWN,
+  STATUSES.CRITICAL,
+  STATUSES.WARNING,
+  STATUSES.SUCCESS,
+];
+
+const statusValues = statuses.reduce(
+  (acc, status, index) => ({
+    ...acc,
+    [status]: index,
+  }),
+  {}
+);
 
 export const SIGNAL_TYPES = {
   SERVICE_LEVEL: 'service_level',
 };
 
 export const signalStatus = (signal) => {
-  if (!signal) return UNKNOWN;
+  if (!signal) return STATUSES.UNKNOWN;
 
   const { type = '' } = signal;
 
@@ -20,5 +33,13 @@ export const signalStatus = (signal) => {
     return serviceLevelStatus({ attainment, target });
   }
 
-  return UNKNOWN;
+  return STATUSES.UNKNOWN;
+};
+
+export const statusFromStatuses = (statusesArray = []) => {
+  const valuesArray = statusesArray.map(
+    ({ status } = STATUSES.UNKNOWN) => statusValues[status] || 0
+  );
+  const leastStatusValue = valuesArray.length ? Math.min(...valuesArray) : 0;
+  return statuses[leastStatusValue];
 };
