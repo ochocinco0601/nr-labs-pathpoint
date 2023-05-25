42a2621e0c7551625694ec5302907e1081ea92d6
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2023-05-25 14:10:37 -0700

    refactor: breaking into sub-components

--- src/components/kpi-modal/index.js
+++ src/components/kpi-modal/index.js
@@ -1,13 +1,7 @@
 import React, { useContext, useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import {
-  Button,
-  EmptyState,
-  HeadingText,
-  Modal,
-  PlatformStateContext,
-} from 'nr1';
+import { Button, HeadingText, Modal, PlatformStateContext } from 'nr1';
 
 import {
   EditInPlace,
@@ -15,40 +9,10 @@ import {
   SimpleBillboard,
 } from '@newrelic/nr-labs-components';
 
+import KpiModalDeleteContent from './delete';
+import KpiModalEmptyState from './empty';
 import { useFetchKpis } from '../../hooks';
-
-const kpiEmptyState = (props) => {
-  const accountValid =
-    props.accountId > 0 && props.accountId !== 'cross-account';
-
-  const desc = !accountValid
-    ? 'At least one account must be selected'
-    : !props.nrqlQuery
-    ? 'Enter and run a query to preview the result'
-    : props.error && props.error.graphQLErrors[0].message;
-
-  return (
-    <EmptyState
-      fullWidth
-      iconType={EmptyState.ICON_TYPE.INTERFACE__PLACEHOLDERS__ICON_PLACEHOLDER}
-      title={
-        !accountValid || !props.nrqlQuery
-          ? 'No preview available yet'
-          : 'Error!'
-      }
-      description={desc}
-      type={
-        !accountValid || !props.nrqlQuery
-          ? EmptyState.TYPE.NORMAL
-          : EmptyState.TYPE.ERROR
-      }
-      additionalInfoLink={{
-        label: 'See our NRQL reference',
-        to: 'https://docs.newrelic.com/docs/query-your-data/nrql-new-relic-query-language/get-started/nrql-syntax-clauses-functions/',
-      }}
-    />
-  );
-};
+import { KPI_MODES } from '../../constants';
 
 const KpiModal = ({
   kpi = {},
@@ -58,14 +22,9 @@ const KpiModal = ({
   setShowModal = setShowModal ? setShowModal : () => null,
   updateKpiArray = updateKpiArray ? updateKpiArray : () => null,
 }) => {
-  const { accountId: platformAcctId } =
-    useContext(PlatformStateContext).accountId;
+  const { accountId: platformAcctId } = useContext(PlatformStateContext);
   const [accountId, setAccountId] = useState(
-    platformAcctId > 0
-      ? platformAcctId
-      : kpi?.accountIds?.length
-      ? kpi.accountIds[0]
-      : ''
+    Number.isInteger(platformAcctId) ? platformAcctId : ''
   );
 
   const [name, setName] = useState(
@@ -80,9 +39,10 @@ const KpiModal = ({
   const nameRef = useRef('name');
 
   useEffect(() => {
+    setAccountId(kpi.accountIds?.length ? kpi.accountIds[0] : '');
     ['add', 'edit'].includes(kpiMode) && setName(kpi.name);
     ['add', 'edit'].includes(kpiMode) && setNrqlQuery(kpi.nrqlQuery);
-  }, [kpi]);
+  }, [kpi, kpiMode]);
 
   const hookData = useFetchKpis({
     kpiData:
@@ -113,134 +73,114 @@ const KpiModal = ({
   return (
     <Modal hidden={!showModal} onClose={() => setShowModal(false)}>
       <div className="modal-component">
-        <div
-          className={`${kpiMode !== 'delete' && 'modal-component-kpi-title'}`}
-        ></div>
-        <div className={`${kpiMode !== 'delete' && 'modal-component-detail'}`}>
-          {kpiMode === 'delete' ? (
-            <div>
-              <HeadingText type={HeadingText.TYPE.HEADING_3}>
-                Are you sure you want to delete &quot;{kpi.name}&quot; KPI ?
-              </HeadingText>
+        {kpiMode === KPI_MODES.DELETE ? (
+          <KpiModalDeleteContent
+            kpi={kpi}
+            kpiIndex={kpiIndex}
+            setShowModal={setShowModal}
+            updateKpiArray={updateKpiArray}
+          />
+        ) : (
+          <>
+            <div className="modal-component-kpi-title"></div>
+            <div className="modal-component-detail">
+              <>
+                <div className="modal-component-edit-in-place">
+                  <div className="modal-component-kpi-name">
+                    <EditInPlace
+                      value={name}
+                      setValue={setName}
+                      ref={nameRef}
+                      placeholder="Enter KPI name"
+                    />
+                  </div>
+                </div>
+                <div className="modal-component-nrql-editor">
+                  <NrqlEditor
+                    query={kpi.nrqlQuery}
+                    accountId={accountId}
+                    saveButtonText="Preview"
+                    onSave={(res) => {
+                      setAccountId(res.accountId);
+                      setNrqlQuery(res.query);
+                    }}
+                  />
+                </div>
+                <div className="modal-component-preview-heading">
+                  <HeadingText type={HeadingText.TYPE.HEADING_1}>
+                    Preview:
+                  </HeadingText>
+                </div>
+                {!accountId ||
+                accountId === 'cross-account' ||
+                !nrqlQuery ||
+                hookData?.error ? (
+                  <div className="modal-component-empty-state">
+                    <KpiModalEmptyState
+                      accountId={accountId}
+                      nrqlQuery={nrqlQuery}
+                      error={hookData?.error}
+                    />
+                  </div>
+                ) : (
+                  <div className="kpi-data">
+                    <SimpleBillboard
+                      metric={{
+                        value: (hookData?.kpis || [])[0]?.value,
+                        previousValue: (hookData?.kpis || [])[0]?.previousValue,
+                        className: 'modal-component-metric-value',
+                      }}
+                      statusTrend={{
+                        className: 'modal-component-status-trend',
+                      }}
+                      title={{
+                        name: name,
+                        className: 'modal-component-metric-name',
+                      }}
+                    />
+                  </div>
+                )}
+              </>
             </div>
-          ) : (
-            <>
-              <div className="modal-component-edit-in-place">
-                <div className="modal-component-kpi-name">
-                  <EditInPlace
-                    value={name}
-                    setValue={setName}
-                    ref={nameRef}
-                    placeholder="Enter KPI name"
-                  />
-                </div>
-              </div>
-              <div className="modal-component-nrql-editor">
-                <NrqlEditor
-                  query={kpi.nrqlQuery}
-                  accountId={accountId}
-                  saveButtonText="Preview"
-                  onSave={(res) => {
-                    setAccountId(res.accountId);
-                    setNrqlQuery(res.query);
-                  }}
-                />
-              </div>
-              <div className="modal-component-preview-heading">
-                <HeadingText type={HeadingText.TYPE.HEADING_1}>
-                  Preview:
-                </HeadingText>
-              </div>
-              {!accountId ||
-              accountId === 'cross-account' ||
-              !nrqlQuery ||
-              hookData?.error ? (
-                <div className="modal-component-empty-state">
-                  {kpiEmptyState({
-                    accountId,
-                    nrqlQuery,
-                    error: hookData?.error,
-                  })}
-                </div>
-              ) : (
-                <div className="kpi-data">
-                  <SimpleBillboard
-                    metric={{
-                      value: (hookData?.kpis || [])[0]?.value,
-                      previousValue: (hookData?.kpis || [])[0]?.previousValue,
-                      className: 'modal-component-metric-value',
-                    }}
-                    statusTrend={{
-                      className: 'modal-component-status-trend',
-                    }}
-                    title={{
-                      name: name,
-                      className: 'modal-component-metric-name',
-                    }}
-                  />
-                </div>
-              )}
-            </>
-          )}
-        </div>
-        <div className="kpi-modal-button-bar">
-          <Button
-            className={`kpi-modal-buttons ${
-              kpiMode === 'add' ? 'add' : kpiMode === 'edit' ? 'save' : 'delete'
-            }`}
-            type={
-              kpiMode === 'delete'
-                ? Button.TYPE.DESTRUCTIVE
-                : Button.TYPE.PRIMARY
-            }
-            sizeType={Button.SIZE_TYPE.LARGE}
-            spacingType={[Button.SPACING_TYPE.EXTRA_LARGE]}
-            disabled={!previewOk}
-            onClick={() => {
-              switch (kpiMode) {
-                case 'add':
-                  updateKpiArray({
-                    id: kpi.id,
-                    accountIds: [accountId],
-                    name,
-                    nrqlQuery,
-                  });
-                  break;
-                case 'edit':
-                  updateKpiArray(
-                    {
-                      id: kpi.id,
-                      accountIds: [accountId],
-                      name,
-                      nrqlQuery,
-                    },
-                    kpiIndex
-                  );
-                  break;
-                case 'delete':
-                  updateKpiArray(kpiIndex);
-              }
-              setShowModal(false);
-            }}
-          >
-            {kpiMode === 'add'
-              ? 'Add KPI'
-              : kpiMode === 'edit'
-              ? 'Save changes'
-              : 'Delete KPI'}
-          </Button>
-          {kpiMode === 'delete' && (
-            <Button
-              className={`kpi-modal-buttons cancel`}
-              type={Button.TYPE.NORMAL}
-              sizeType={Button.SIZE_TYPE.LARGE}
-              spacingType={[Button.SPACING_TYPE.EXTRA_LARGE]}
-              onClick={() => setShowModal(false)}
-            >
-              Cancel
-            </Button>
-          )}
-        </div>
+            <div className="kpi-modal-button-bar">
+              <Button
+                className={`kpi-modal-buttons ${
+                  kpiMode === KPI_MODES.ADD ? 'add' : 'save'
+                }`}
+                type={Button.TYPE.PRIMARY}
+                sizeType={Button.SIZE_TYPE.LARGE}
+                spacingType={[Button.SPACING_TYPE.EXTRA_LARGE]}
+                disabled={!previewOk}
+                onClick={() => {
+                  switch (kpiMode) {
+                    case KPI_MODES.ADD:
+                      updateKpiArray({
+                        id: kpi.id,
+                        accountIds: [accountId],
+                        name,
+                        nrqlQuery,
+                      });
+                      break;
+                    case KPI_MODES.EDIT:
+                      updateKpiArray(
+                        {
+                          id: kpi.id,
+                          accountIds: [accountId],
+                          name,
+                          nrqlQuery,
+                        },
+                        kpiIndex
+                      );
+                      break;
+                  }
+                  setShowModal(false);
+                }}
+              >
+                {kpiMode === KPI_MODES.ADD ? 'Add KPI' : 'Save changes'}
+              </Button>
+            </div>
+          </>
+        )}
       </div>
     </Modal>
   );
@@ -249,7 +189,7 @@ const KpiModal = ({
 KpiModal.propTypes = {
   kpi: PropTypes.object,
   kpiIndex: PropTypes.number,
-  kpiMode: PropTypes.string,
+  kpiMode: PropTypes.oneOf(Object.values(KPI_MODES)),
   showModal: PropTypes.bool,
   setShowModal: PropTypes.func,
   updateKpiArray: PropTypes.func,
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,4 @@ export * from './nerd-storage';
 export * from './modes';
 export * from './statuses';
 export * from './signals';
+export * from './kpis';
