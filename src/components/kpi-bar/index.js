7bfe9c70568cad088d34bfbffa62d2bb42938f0f
shahram <shahram@newrelic.com>
2023-05-25 17:51:39 -0700

    feat: add kpi-bar

---
+++ src/components/kpi-bar/index.js
@@ -0,0 +1,206 @@
+import React, { useCallback, useContext, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import { EmptyState, PlatformStateContext, Button } from 'nr1';
+
+import { KpiModal } from '../';
+import { SimpleBillboard } from '@newrelic/nr-labs-components';
+
+import { useFetchKpis } from '../../hooks';
+import { KPI_MODES } from '../../constants';
+
+const KpiBar = ({
+  nerdletMode = 'view', // valid modes: view, edit
+  kpiArray = [],
+  setKpiArray = setKpiArray ? setKpiArray : () => null,
+}) => {
+  const { accountId } = useContext(PlatformStateContext);
+
+  const [showModal, setShowModal] = useState(false);
+  const [currentKpi, setCurrentKpi] = useState({});
+  const [kpiMode, setKpiMode] = useState(KPI_MODES.VIEW);
+  const [kpiIndex, setKpiIndex] = useState(-1);
+
+  useEffect(() => {
+    if (kpiMode === KPI_MODES.VIEW) {
+      updateKpis(kpiArray);
+    }
+  }, [kpiMode]);
+
+  const [queryResults, setQueryResults] = useState([]);
+
+  const hookData = useFetchKpis({ kpiData: kpiArray });
+
+  useEffect(() => {
+    if (hookData.kpis && hookData.kpis.length) {
+      setQueryResults(hookData.kpis);
+    }
+  }, [hookData]);
+
+  const updateKpis = useCallback((updatedKpiArray) => {
+    setKpiIndex(-1);
+    setShowModal(false);
+    setKpiMode(KPI_MODES.VIEW);
+    setKpiArray(updatedKpiArray);
+  });
+
+  const deleteKpi = useCallback((index) => {
+    updateKpis(kpiArray.filter((_, i) => i !== index));
+  });
+
+  const updateKpi = useCallback((updatedKpi, kpiIndex) => {
+    const newKpis = kpiArray.map((k, i) => {
+      if (i === kpiIndex) {
+        return { ...k, ...updatedKpi };
+      } else {
+        return k;
+      }
+    });
+    updateKpis(newKpis);
+  });
+
+  const addNewKpi = useCallback(
+    (currentKpi) => {
+      currentKpi.accountIds = [Number(currentKpi.accountIds)];
+      const newKpiArray = [...kpiArray, currentKpi];
+      updateKpis(newKpiArray);
+    },
+    [kpiArray]
+  );
+
+  return (
+    <div className="kpi-bar">
+      <div className="kpi-bar-heading">
+        {nerdletMode === 'view' ? (
+          <div className="kpi-bar-title buttonEditModeWidth">
+            <label>Critical Measures</label>
+          </div>
+        ) : (
+          <div>
+            <div className="kpi-bar-edit-mode-title buttonEditModeWidth">
+              <label>Critical Measures</label>
+            </div>
+            <div className="kpi-bar-add-button">
+              <Button
+                type={Button.TYPE.SECONDARY}
+                iconType={Button.ICON_TYPE.INTERFACE__SIGN__PLUS__V_ALTERNATE}
+                sizeType={Button.SIZE_TYPE.LARGE}
+                onClick={() => {
+                  setKpiIndex(kpiArray.length); // new kpiArray bucket being added
+                  setCurrentKpi({
+                    id: kpiArray.length
+                      ? kpiArray[kpiArray.length - 1].id + 1
+                      : 0,
+                    accountIds: [accountId],
+                    name: '',
+                    nrqlQuery: '',
+                  });
+                  setKpiMode(KPI_MODES.ADD);
+                  setShowModal(true);
+                }}
+              >
+                Create new KPI
+              </Button>
+            </div>
+            <div id="kpi-modal">
+              {kpiMode !== KPI_MODES.VIEW && (
+                <KpiModal
+                  kpi={currentKpi}
+                  kpiIndex={kpiIndex}
+                  kpiMode={kpiMode} // kpiMode = "view" / "add" kpi / "edit" existing kpi / "delete" existing kpi
+                  showModal={showModal}
+                  setShowModal={setShowModal}
+                  updateKpiArray={
+                    kpiMode === KPI_MODES.ADD
+                      ? addNewKpi
+                      : kpiMode === KPI_MODES.EDIT
+                      ? updateKpi
+                      : deleteKpi
+                  }
+                />
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div
+        className={`kpi-containers ${
+          nerdletMode === 'edit'
+            ? 'kpiBarEditModeMaxWidth'
+            : 'kpiBarViewModeMaxWidth'
+        }`}
+      >
+        {!kpiArray || !kpiArray.length ? (
+          <div className="empty-state-component">
+            <EmptyState
+              fullWidth
+              title="No KPIs available"
+              type={EmptyState.TYPE.NORMAL}
+            />
+          </div>
+        ) : (
+          kpiArray.map((kpi, index) => (
+            <div
+              key={index}
+              className={`kpi-container ${
+                nerdletMode === 'edit'
+                  ? 'kpiContainerEditModeWidth'
+                  : 'kpiContainerViewModeWidth'
+              }`}
+            >
+              <div className="kpi-data">
+                <SimpleBillboard
+                  metric={{
+                    value: ((queryResults || [])[index] || {}).value || 0,
+                    previousValue:
+                      ((queryResults || [])[index] || {}).previousValue || '',
+                  }}
+                  title={{
+                    name: kpi.name,
+                  }}
+                />
+              </div>
+              {nerdletMode === 'edit' && (
+                <div className="kpi-buttons">
+                  <Button
+                    className="box-shadow"
+                    type={Button.TYPE.SECONDARY}
+                    iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__CLOSE}
+                    sizeType={Button.SIZE_TYPE.SMALL}
+                    onClick={() => {
+                      setKpiIndex(index); // kpi array bucket being deleted
+                      setCurrentKpi(kpi);
+                      setKpiMode(KPI_MODES.DELETE);
+                      setShowModal(true);
+                    }}
+                  />
+                  <Button
+                    className="box-shadow"
+                    type={Button.TYPE.SECONDARY}
+                    iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__EDIT}
+                    sizeType={Button.SIZE_TYPE.SMALL}
+                    onClick={() => {
+                      setKpiIndex(index); // kpiArray bucket being edited
+                      setCurrentKpi(kpi);
+                      setKpiMode(KPI_MODES.EDIT);
+                      setShowModal(true);
+                    }}
+                  />
+                </div>
+              )}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+KpiBar.propTypes = {
+  nerdletMode: PropTypes.string,
+  kpiArray: PropTypes.object,
+  setKpiArray: PropTypes.func,
+};
+
+export default KpiBar;
