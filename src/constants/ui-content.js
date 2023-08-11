f5df59130831e8e2fd0c3fe4310422938f17e5a7
shahram <shahram@newrelic.com>
2023-08-11 11:15:55 -0700

    refactor: add UI content json file

---
+++ src/constants/ui-content.js
@@ -0,0 +1,86 @@
+export const UI_CONTENT = {
+  GLOBAL: {
+    BUTTON_LABEL_CREATE_FLOW: 'Create new flow',
+    BUTTON_LABEL_EDIT_MODE: 'Edit Mode',
+    BUTTON_LABEL_EXIT_EDIT_MODE: 'Done Editing',
+    BUTTON_LABEL_INLINE_MODE: 'Inline',
+    BUTTON_LABEL_STACKED_MODE: 'Stacked',
+  },
+  FLOW: {
+    //
+  },
+  STAGE: {
+    //
+  },
+  LEVEL: {
+    //
+  },
+  STEP: {
+    //
+  },
+  KPI_BAR: {
+    TITLE: 'Flow KPIs',
+  },
+  GET_STARTED: {
+    HEADING: 'Pathpoint',
+    DESCRIPTION:
+      'Pathpoint is an enterprise platform tracker that offers a unique approach to business journey observability. It models system health in relation to actual user-impacting business stages.',
+    BUTTON_LABEL_GET_STARTED: 'Get started',
+    BUTTON_LABEL_GO_BACK: 'Go back',
+    LINK_LABEL: 'See our docs',
+    LINK_URL: 'https://github.com/newrelic/hedgehog/...',
+  },
+  FLOW_OVERVIEW: {
+    //
+  },
+  GUIDED_VIEW: {
+    FLOW: {
+      TITLE: 'This is a demo flow',
+      DESCRIPTION:
+        "A flow maps an organization's customer journey, optimizing business observability.",
+      BUTTON_LABEL_SKIP_TOUR: 'Skip tour',
+      BUTTON_LABEL_PREVIOUS: 'Previous',
+      BUTTON_LABEL_NEXT: 'Next',
+    },
+    STAGE: {
+      TITLE: 'This is a stage.',
+      DESCRIPTION:
+        'A stage represents a high-level functional category in the flow, which consists of multiple steps to complete.',
+      BUTTON_LABEL_SKIP_TOUR: 'Skip tour',
+      BUTTON_LABEL_PREVIOUS: 'Previous',
+      BUTTON_LABEL_NEXT: 'Next',
+    },
+    LEVEL: {
+      TITLE: 'This is a level',
+      DESCRIPTION:
+        'A level collects the potential alternate paths a user may traverse as they progress through this level of the stage. Levels are depicted in numerical order.',
+      BUTTON_LABEL_SKIP_TOUR: 'Skip tour',
+      BUTTON_LABEL_PREVIOUS: 'Previous',
+      BUTTON_LABEL_NEXT: 'Next',
+    },
+    STEP: {
+      TITLE: 'This is a step',
+      DESCRIPTION:
+        'A step is an individual gate in the path, usually representing in aggregate an underlying system. Click to expand the step.',
+      BUTTON_LABEL_SKIP_TOUR: 'Skip tour',
+      BUTTON_LABEL_PREVIOUS: 'Previous',
+      BUTTON_LABEL_NEXT: 'Next',
+    },
+    SIGNAL: {
+      TITLE: 'This is a signal',
+      DESCRIPTION:
+        'A signal is a service level that reflects the underlying health of its parent step.',
+      BUTTON_LABEL_SKIP_TOUR: 'Skip tour',
+      BUTTON_LABEL_PREVIOUS: 'Previous',
+      BUTTON_LABEL_NEXT: 'Next',
+    },
+    FLOW_KPIS: {
+      TITLE: 'These are flow KPIs.',
+      DESCRIPTION:
+        'A flow KPI lets you measure attainment against a target business objective of the overall flow.',
+      BUTTON_LABEL_SKIP_TOUR: 'Skip tour',
+      BUTTON_LABEL_PREVIOUS: 'Previous',
+      BUTTON_LABEL_NEXT: 'Next',
+    },
+  },
+};
