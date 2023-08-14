606bd8268d05908e75af731062e588df215307b1
shahram <shahram@newrelic.com>
2023-08-14 11:43:48 -0700

    chore: remove blue edit-mode banner

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,5 +2,3 @@ export { default as useServiceLevelsSearch } from './use-service-levels-search';
 export { default as useFetchServiceLevels } from './use-fetch-service-levels';
 export { default as useFetchKpis } from './use-fetch-kpis';
 export { default as useFlowLoader } from './use-flow-loader';
-export { default as useFetchUserConfig } from './use-fetch-user-config';
-export { default as useUpdateUserConfig } from './use-update-user-config';
