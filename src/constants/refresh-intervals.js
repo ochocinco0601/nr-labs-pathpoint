eef8fda940a0055080a12827ca33c80d965dde73
shahramk <shahram@newrelic.com>
2023-12-26 17:50:25 -0800

    feat: add flow refresh interval constant

---
+++ src/constants/refresh-intervals.js
@@ -0,0 +1,6 @@
+export const REFRESH_INTERVALS = [
+  { value: '5', label: '5 seconds' },
+  { value: '10', label: '10 seconds' },
+  { value: '15', label: '15 seconds' },
+  { value: '60', label: '1 minute' },
+];
