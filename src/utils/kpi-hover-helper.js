31884188a5fef649b06716d9166f0c7a30335fc3
shahramk <shahram@newrelic.com>
2024-01-09 10:36:36 -0800

    refactor: add date validation

--- src/utils/kpi-hover-helper.js
+++ src/utils/kpi-hover-helper.js
@@ -1,11 +1,20 @@
-export const formatKpiHoverDatime = (dateClause) =>
-  !dateClause
-    ? null
-    : dateClause.startsWith("'")
-    ? new Intl.DateTimeFormat('default', {
+export const formatKpiHoverDatime = (dateClause) => {
+  if (!dateClause) {
+    return '';
+  } else if (dateClause.startsWith("'")) {
+    let date;
+    try {
+      date = new Date(dateClause.slice(1, -1));
+      return new Intl.DateTimeFormat('default', {
         dateStyle: 'medium',
         timeStyle: 'short',
       })
-        .format(new Date(dateClause.substring(1, dateClause.length - 1)))
-        .replace(/[APM]{2}/, (match) => match.toLowerCase())
-    : dateClause.toLowerCase();
+        .format(date)
+        .replace(/[APM]{2}/, (match) => match.toLowerCase());
+    } catch (err) {
+      return dateClause;
+    }
+  } else {
+    return dateClause.toLowerCase();
+  }
+};
