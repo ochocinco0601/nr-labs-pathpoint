879cede114272a088c6ed68c7897f5651d82b279
shahramk <shahram@newrelic.com>
2024-02-01 18:56:09 -0800

    feat: create status icon grid layout to present entity and alert statuses separately

---
+++ src/components/signals-grid-layout/index.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import IconsLib from '../icons-lib';
+import { SIGNAL_TYPES } from '../../constants';
+
+const SignalsGridLayout = ({ statuses }) => {
+  const [entityGrid, setEntityGrid] = useState(null);
+  const [alertGrid, setAlertGrid] = useState(null);
+  const [width, setWidth] = useState(null);
+  const wrapperRef = useRef();
+
+  useEffect(() => {
+    if (statuses) {
+      setEntityGrid(
+        statuses
+          .filter((s) => s.type === SIGNAL_TYPES.ENTITY)
+          .map(({ style, type, status, ...statusProps }, i) => (
+            <IconsLib
+              key={i}
+              className={status}
+              type={type}
+              {...statusProps}
+              style={{ style, margin: 1, marginBottom: -3 }}
+            />
+          ))
+      );
+      setAlertGrid(
+        statuses
+          .filter((s) => s.type === SIGNAL_TYPES.ALERT)
+          .map(({ style, type, status, ...statusProps }, i) => (
+            <IconsLib
+              key={i}
+              className={status}
+              type={type}
+              {...statusProps}
+              style={{ style, margin: 1, marginBottom: -3 }}
+            />
+          ))
+      );
+    }
+  }, [statuses]);
+
+  useLayoutEffect(() => {
+    const { width } = wrapperRef.current.getBoundingClientRect();
+    setWidth(width);
+  }, []);
+
+  return (
+    <div className="new-status-icons-wrapper" ref={wrapperRef}>
+      <div className="new-status-icons-container" style={{ width }}>
+        {entityGrid}
+      </div>
+      <div className="new-status-icons-container" style={{ width }}>
+        {alertGrid}
+      </div>
+    </div>
+  );
+};
+
+SignalsGridLayout.propTypes = {
+  statuses: PropTypes.array,
+};
+
+export default SignalsGridLayout;
