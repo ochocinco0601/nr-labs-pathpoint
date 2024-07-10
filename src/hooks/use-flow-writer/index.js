90991ecced5cb55f176397220ee765e5704c1825
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2024-07-09 17:28:30 -0700

    fix: delete flow bug

--- src/hooks/use-flow-writer/index.js
+++ src/hooks/use-flow-writer/index.js
@@ -3,9 +3,40 @@ import { AccountStorageMutation, AccountStorageQuery } from 'nr1';
 
 import { NERD_STORAGE } from '../../constants';
 
+const log = async ({ accountId, documentId, user }) => {
+  const { data, error } = await AccountStorageQuery.query({
+    accountId,
+    collection: NERD_STORAGE.EDITS_LOG_COLLECTION,
+    documentId,
+  });
+  if (error) {
+    console.error('Error reading flow log', error);
+    return;
+  }
+
+  const document = {
+    logs: [
+      ...(data?.logs || []),
+      {
+        timestamp: Date.now(),
+        user,
+      },
+    ],
+  };
+  const { error: logErr } = await AccountStorageMutation.mutate({
+    accountId,
+    actionType: AccountStorageMutation.ACTION_TYPE.WRITE_DOCUMENT,
+    collection: NERD_STORAGE.EDITS_LOG_COLLECTION,
+    documentId,
+    document,
+  });
+
+  if (logErr) console.error('Error writing log', logErr);
+};
+
 const useFlowWriter = ({ accountId, user }) => {
   const write = useCallback(async ({ documentId, document }) => {
-    const { data: { nerdStorageWriteDocument } = {}, error: flowWriteError } =
+    const { data: { nerdStorageWriteDocument } = {}, error } =
       await AccountStorageMutation.mutate({
         actionType: AccountStorageMutation.ACTION_TYPE.WRITE_DOCUMENT,
         collection: NERD_STORAGE.FLOWS_COLLECTION,
@@ -14,40 +45,34 @@ const useFlowWriter = ({ accountId, user }) => {
         document: document,
       });
 
-    if (flowWriteError) {
-      console.error(flowWriteError);
+    if (error) {
+      console.error('Error writing to flow', error);
       return;
     }
 
-    if (nerdStorageWriteDocument) {
-      const { data: logsData, error: logReadError } =
-        await AccountStorageQuery.query({
-          accountId,
-          collection: NERD_STORAGE.EDITS_LOG_COLLECTION,
-          documentId,
-        });
-      if (logReadError) return;
-
-      const { logs = [] } = logsData || {};
-      AccountStorageMutation.mutate({
-        accountId,
-        actionType: AccountStorageMutation.ACTION_TYPE.WRITE_DOCUMENT,
-        collection: NERD_STORAGE.EDITS_LOG_COLLECTION,
-        documentId,
-        document: {
-          logs: [
-            ...logs,
-            {
-              timestamp: Date.now(),
-              user,
-            },
-          ],
-        },
-      });
-    }
+    if (nerdStorageWriteDocument) log({ accountId, documentId, user });
   }, []);
 
-  return { write };
+  const deleteFlow = useCallback(async (id) => {
+    const documentId = id;
+    const { data: { nerdStorageDeleteDocument } = {}, error } =
+      await AccountStorageMutation.mutate({
+        actionType: AccountStorageMutation.ACTION_TYPE.DELETE_DOCUMENT,
+        collection: NERD_STORAGE.FLOWS_COLLECTION,
+        accountId,
+        documentId,
+      });
+
+    if (error) {
+      console.error('Error deleting flow', error);
+      return;
+    }
+
+    if (nerdStorageDeleteDocument?.deleted)
+      log({ accountId, documentId, user });
+  }, []);
+
+  return { write, deleteFlow };
 };
 
 export default useFlowWriter;
