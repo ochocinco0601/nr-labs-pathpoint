84c1fef137f256f50be5f6e7aa6f832f949370ed
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2024-11-15 14:20:39 -0800

    style: update message for alerting service level with no incidents

--- src/constants/ui-content.js
+++ src/constants/ui-content.js
@@ -137,7 +137,7 @@ export const UI_CONTENT = {
       NO_RECENT_INCIDENTS: 'No open incidents to display.',
       LATEST_RECENT_INCIDENT:
         'No open incidents. Showing the most recent incident.',
-      ALERTING_SL_NO_INCIDENT: 'No incidents for this alerting service level!',
+      ALERTING_SL_NO_INCIDENT: 'This Service Level is in an unhealthy state, however we can\'t find any correlated incidents. It is likely that the Service Level is out of compliance - to investigate further, please open the entity view (linked above).',
     },
     TOOLTIP: {
       WORKLOAD_UNKNOWN: 'The status of this workload is unknown',
